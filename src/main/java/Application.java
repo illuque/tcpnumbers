3e1a3dc8b015e416a478a8a54d1b7bd8308c3e58
Iago Lluque <iago.lluque@gmail.com>
2021-09-12 20:35:58 +0200

    Concurrency improvements with Concurrent collection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,32 +2,37 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Application {
 
     private static final String TERMINATION_SEQUENCE = "terminate";
-    public static final int PORT = 4000;
-    public static final int MAX_CLIENTS = 5;
+    private static final int PORT = 4000;
+    private static final int MAX_CLIENTS = 5;
 
     public static void main(String[] args) throws IOException {
-        Set<Integer> uniqueNumbersSet = Collections.synchronizedSet(new HashSet<>());
-        FileWriter fileWriter = new FileWriter(generateNewFile(), true);
-        LinesReader linesReader = LinesReader.create(TERMINATION_SEQUENCE, uniqueNumbersSet, fileWriter);
+        try (FileWriter fileWriter = new FileWriter(generateNewFile(), true)) {
+            Set<Integer> uniqueNumbersSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-        TcpServer tcpServer = TcpServer.create(PORT, MAX_CLIENTS, linesReader);
+            LinesReader linesReader = LinesReader.create(TERMINATION_SEQUENCE, uniqueNumbersSet, fileWriter);
 
-        tcpServer.start();
+            TcpServer tcpServer = TcpServer.create(PORT, MAX_CLIENTS, linesReader);
+
+            tcpServer.start();
+        }
     }
 
     private static File generateNewFile() throws IOException {
         File outputFile = new File("numbers.log").getAbsoluteFile();
-        if (outputFile.exists()) {
-            outputFile.delete();
+
+        if (outputFile.exists() && !outputFile.delete()) {
+            throw new IllegalStateException("Could not create file");
         }
 
-        outputFile.createNewFile();
+        if (!outputFile.createNewFile()) {
+            throw new IllegalStateException("Could not create file");
+        }
 
         return outputFile;
     }
